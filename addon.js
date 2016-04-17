56e24e0c83798f4168e8532d6791bb8eef025a50
omaximov <omaximov@griddynamics.com>
2016-04-18 00:56:56 +0300

    some more

--- addon.js
+++ addon.js
@@ -1,9 +1,10 @@
 var addon = require('bindings')('addon');
 
-var radio = new addon.nrf24(22,7);
+var radio = new addon.nrf24(22,7,32);
 radio.begin();
 radio.enableDynamicPayloads();
-radio.setRetries(5,15);
+radio.setPayloadSize(8);
+radio.setRetries(15,15);
 radio.printDetails();
 radio.openWritingPipe(0xF0F0F0F0E1);
 radio.openReadingPipe(1,0xF0F0F0F0D2);
